2a2a1aa5584ef7dfc87fc062b7e666abaa8525bf
flaviecousin <flaviecousin29@gmail.com>
2026-08-26 16:42:11 +0200

    Licenses of the icons put back in the codes

--- app/src/main/java/com/example/cookingbook/ui/icons/MoonIcon.kt
+++ app/src/main/java/com/example/cookingbook/ui/icons/MoonIcon.kt
@@ -6,6 +6,29 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 
+/*
+MIT License
+
+Copyright (c) 2020 Phosphor Icons
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 val PhosphorMoon: ImageVector
     get() {
         if (_PhosphorMoon != null) return _PhosphorMoon!!
@@ -20,20 +43,23 @@ val PhosphorMoon: ImageVector
             path(
                 fill = SolidColor(Color.Black)
             ) {
-                moveTo(232.13f, 143.64f)
-                arcToRelative(6f, 6f, 0f, false, false, -6f, -1.49f)
-                arcTo(90.07f, 90.07f, 0f, false, true, 113.86f, 29.85f)
-                arcToRelative(6f, 6f, 0f, false, false, -7.49f, -7.48f)
-                arcTo(102.88f, 102.88f, 0f, false, false, 54.48f, 58.68f)
-                arcTo(102f, 102f, 0f, false, false, 197.32f, 201.52f)
-                arcToRelative(102.88f, 102.88f, 0f, false, false, 36.31f, -51.89f)
-                arcTo(6f, 6f, 0f, false, false, 232.13f, 143.64f)
+                moveTo(233.54f, 142.23f)
+                arcToRelative(8f, 8f, 0f, false, false, -8f, -2f)
+                arcToRelative(88.08f, 88.08f, 0f, false, true, -109.8f, -109.8f)
+                arcToRelative(8f, 8f, 0f, false, false, -10f, -10f)
+                arcToRelative(104.84f, 104.84f, 0f, false, false, -52.91f, 37f)
+                arcTo(104f, 104f, 0f, false, false, 136f, 224f)
+                arcToRelative(103.09f, 103.09f, 0f, false, false, 62.52f, -20.88f)
+                arcToRelative(104.84f, 104.84f, 0f, false, false, 37f, -52.91f)
+                arcTo(8f, 8f, 0f, false, false, 233.54f, 142.23f)
                 close()
-                moveToRelative(-42f, 48.29f)
-                arcToRelative(90f, 90f, 0f, false, true, -126f, -126f)
-                arcTo(90.9f, 90.9f, 0f, false, true, 99.65f, 37.66f)
-                arcTo(102.06f, 102.06f, 0f, false, false, 218.34f, 156.35f)
-                arcTo(90.9f, 90.9f, 0f, false, true, 190.1f, 191.93f)
+                moveTo(188.9f, 190.34f)
+                arcTo(88f, 88f, 0f, false, true, 65.66f, 67.11f)
+                arcToRelative(89f, 89f, 0f, false, true, 31.4f, -26f)
+                arcTo(106f, 106f, 0f, false, false, 96f, 56f)
+                arcTo(104.11f, 104.11f, 0f, false, false, 200f, 160f)
+                arcToRelative(106f, 106f, 0f, false, false, 14.92f, -1.06f)
+                arcTo(89f, 89f, 0f, false, true, 188.9f, 190.34f)
                 close()
             }
         }.build()
